26f9a2f18ac1165213205a14794ff4a7e53d684d
Shunskiy <pas4o@yandex.ru>
2023-06-02 12:52:10 +0000

    Deploying to gh-pages from @ Shunskiy/templates-demo@e9741775b1faf494bc00d68b31e3f15c8ab032d6 🚀

--- src/helpers/addPrefixToHref.ts
+++
@@ -1,5 +0,0 @@
-import { ICopyNavbar } from "@/components/CopyNavbar/CopyNavbar"
-
-export default function addPrefixToHref(navbar: ICopyNavbar["content"], prefix: string) {
-    return navbar.map(item => ({ ...item, href: prefix + item.href }));
-}
